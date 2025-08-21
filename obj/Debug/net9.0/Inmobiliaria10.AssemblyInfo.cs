78c0e5a86d2573d8143a2fb01710d5e3cea382fb
Roma <roma.ricchiardi@gmail.com>
2025-08-21 16:52:36 -0300

    edit

--- obj/Debug/net9.0/Inmobiliaria10.AssemblyInfo.cs
+++ obj/Debug/net9.0/Inmobiliaria10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inmobiliaria10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff6a2588094a7758000290269d0f7efe82d3d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949b9690ce5738d58ea3b421d9deb3b315ba377a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inmobiliaria10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inmobiliaria10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
